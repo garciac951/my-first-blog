9c66564ef3fb4d89039cbf02474726dc63e4810f
Carlos <carlos_garcia14@live.com>
2018-04-29 18:47:35 -0600

    debug equals false

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'pn%__v3+2yx08nhem%81_$=%ypo-lcj7v)mhc-g(bcdtnaoy#y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
